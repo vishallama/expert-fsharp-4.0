1477d04a5308f3ba0b3434b71a94d59f92b0b3ad
Vishal Lama <vishal.lama@gmail.com>
2016-11-01 15:38:38 -0600

    Use try...finally construct in an example

--- chapters/04-introducing-imperative-programming/Exceptions.fsx
+++ chapters/04-introducing-imperative-programming/Exceptions.fsx
@@ -14,3 +14,18 @@ let http(url : string) =
         | :? System.Net.WebException -> "web request can't be processed"
 
 printfn "%s" (http("invalid URL"))
+
+// Process exceptions using try...finally... construct. This guarantees to
+// run the finally clause both when an exception is thrown and when the
+// expression evaluates normally. This allows one to ensure that resources
+// are disposed off after the completion of an operation.
+let httpViaTryFinally(url : string) =
+    let request = System.Net.WebRequest.Create(url)
+    let response = request.GetResponse()
+    try
+        let stream = response.GetResponseStream()
+        let reader = new StreamReader(stream)
+        let html = reader.ReadToEnd()
+        html
+    finally
+        response.Close()
