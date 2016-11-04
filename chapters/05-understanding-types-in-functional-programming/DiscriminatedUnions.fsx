cc49fca984e69fac3ee0e804f73c73fb279657a6
Vishal Lama <vishal.lama@gmail.com>
2016-11-04 13:06:24 -0600

    Create Proposition (type)

--- chapters/05-understanding-types-in-functional-programming/DiscriminatedUnions.fsx
+++ chapters/05-understanding-types-in-functional-programming/DiscriminatedUnions.fsx
@@ -18,3 +18,22 @@ let averageSpeed (tr : Transport) =
     | Car _ -> 35
     | Bicycle -> 16
     | Bus _ -> 24
+
+// Discriminated Unions can include recursive references
+type Proposition =
+    | True
+    | And of Proposition * Proposition
+    | Or of Proposition * Proposition
+    | Not of Proposition
+
+// Recursive functions can be used to tranverse such a type
+let rec eval (p : Proposition) =
+    match p with
+    | True -> true
+    | And(p1, p2) -> eval p1 && eval p2
+    | Or(p1, p2) -> eval p1 || eval p2
+    | Not(p1) -> not (eval p1)
+
+let p1 = And(True, Not(True))
+printfn "%b" (eval p1)              // false
+printfn "%b" (eval (Not(p1)))       // true
