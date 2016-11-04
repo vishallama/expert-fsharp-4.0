b0fab5f5eb211768141f78c05f3abaf417134c2b
Vishal Lama <vishal.lama@gmail.com>
2016-11-04 10:02:24 -0600

    Add examples of record label disambiguation

---
+++ chapters/05-understanding-types-in-functional-programming/RecordLabelDisambiguation.fsx
@@ -0,0 +1,17 @@
+type Dot = { X : int; Y : int }
+type Point = { X : float; Y : float }
+type Person = {
+    Name : string
+    DateofBirth : System.DateTime
+}
+
+let coords1 (p : Point) = (p.X, p.Y)
+let coords2 (d : Dot) = (d.X, d.Y)
+
+// use of X and Y implies type "Point", the last definition that fits
+let distance p = sqrt (p.X * p.X + p.Y * p.Y)
+
+// record values with explicit type annotation for the whole record
+let alice =
+    ({ Name = "Alice"
+       DateofBirth = new System.DateTime(1970, 07, 23)} : Person)
