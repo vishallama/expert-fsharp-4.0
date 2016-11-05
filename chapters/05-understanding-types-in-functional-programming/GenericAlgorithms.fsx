2b683bb914c49fa3bafe31b3424ada03820b8f5f
Vishal Lama <vishal.lama@gmail.com>
2016-11-05 17:56:12 -0600

    Convert non-generic function to a generic one

--- chapters/05-understanding-types-in-functional-programming/GenericAlgorithms.fsx
+++ chapters/05-understanding-types-in-functional-programming/GenericAlgorithms.fsx
@@ -6,3 +6,19 @@ let rec hcf a b =
 
 printfn "%d" (hcf 18 12)        // 6
 printfn "%d" (hcf 33 24)        // 3
+
+// The above algorithm works only over integers.
+let hcfGeneric (zero, sub, lessThan) =
+    let rec hcf a b =
+        if a = zero then b
+        elif lessThan a b then hcf a (sub b a)
+        else hcf (sub a b) b
+    hcf
+
+let hcfInt = hcfGeneric (0, (-), (<))
+let hcfInt64 = hcfGeneric (0L, (-), (<))
+let hcfBigInt = hcfGeneric (0I, (-), (<))
+
+printfn "%d" (hcfInt 18 12)        // 6
+printfn "%d" (hcfInt 33 24)        // 3
+printfn "%A" (hcfBigInt 1810287116162232383039576I 1239028178293092830480239032I)        // 33224
